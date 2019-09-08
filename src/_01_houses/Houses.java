d4ab45596e9dd8abb6f8ccf262ad23c2206b7d80
League Student <league@iMac-10.attlocal.net>
2019-09-08 11:23:52 -0700

    level1

---
+++ src/_01_houses/Houses.java
@@ -0,0 +1,12 @@
+package _01_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Houses {
+public static void main(String[] args) {
+	Robot r2d2 = new Robot();
+	s
+	r2d2.penDown();
+
+}
+}
